072b22a685cc353bb4b63141255d88210997e6be
Moritz Schmid <moritz.schmid@me.com>
2021-07-17 14:42:08 +0200

    Add 20 minute adventure from cmd

--- Gti4Programmers/Gti4Programmers/Rick.cpp
+++ Gti4Programmers/Gti4Programmers/Rick.cpp
@@ -4,3 +4,8 @@ void rick()
 {
     std::cout << "This Rick was added from the command line" << std::endl;
 }
+
+void TwentyMinAdventure()
+{
+    std::cout << "Let's do a quick 20 minute adventure. In and out." << std::endl;
+}
