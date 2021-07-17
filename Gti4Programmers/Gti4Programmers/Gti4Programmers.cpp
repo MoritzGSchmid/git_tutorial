c1dc0816b6c910c6ca5ce2a603c53601284cb23e
Moritz Schmid <moritz.schmid@me.com>
2021-07-17 13:50:00 +0200

    some code changes

--- Gti4Programmers/Gti4Programmers/Gti4Programmers.cpp
+++ Gti4Programmers/Gti4Programmers/Gti4Programmers.cpp
@@ -6,6 +6,8 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    auto something{ 7 };
+    std::cout << "Some change for git: " << something << std::endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
